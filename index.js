7baf6677d5b088bec12779666ebe3902e864261c
Eduardo Nicastro <edu.nicastro@gmail.com>
2021-09-26 14:40:23 -0300

    setup logging and body parser

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const app = express();
 // App Setup
-
+app.use(morgan("combined"));
+app.use(bodyParser.json({ type: "*/*" }));
 // Server Setup
 const PORT = process.env.PORT || 3090;
 const server = http.createServer(app);
