80d50291b34f396bc5d1f781a8879b50bc12aca9
Eduardo Nicastro <edu.nicastro@gmail.com>
2021-09-27 23:32:03 -0300

    fix and finish auth server

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
+// Main starting point of the application
 const express = require("express");
 const http = require("http");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
-const mongoose = require("mongoose");
 const app = express();
+const router = require("./router");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
 // DB Setup
 mongoose.connect("mongodb://localhost/auth");
+
 // App Setup
 app.use(morgan("combined"));
+app.use(cors());
 app.use(bodyParser.json({ type: "*/*" }));
+router(app);
+
 // Server Setup
-const PORT = process.env.PORT || 3090;
+const port = process.env.PORT || 3090;
 const server = http.createServer(app);
-server.listen(PORT);
-console.log(`Server listening on port ${PORT}`);
+server.listen(port);
+console.log("Server listening on:", port);
